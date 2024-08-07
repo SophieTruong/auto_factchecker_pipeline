9c658f0fbde8d648914145b2c034076ef9469c27
SophieTruong <sophietruong1992@gmail.com>
2024-08-07 11:26:50 +0300

    Add Postgres pgvector database

---
+++ database/db_seed.py
@@ -0,0 +1,125 @@
+import os
+import time 
+
+import logging
+logger = logging.getLogger(__name__)
+
+import pandas as pd 
+import numpy as np
+
+from sentence_transformers import SentenceTransformer
+
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.orm import sessionmaker,mapped_column, declarative_base
+from sqlalchemy.sql import expression
+from sqlalchemy.types import DateTime
+from sqlalchemy.ext.compiler import compiles
+
+from pgvector.sqlalchemy import Vector
+
+
+## TODO: use arguments
+MODEL = 'msmarco-distilbert-base-dot-prod-v3'
+DATAPATH = "./data"
+FILENAME = "politifact_factcheck_data.json"
+
+Base = declarative_base()
+N_DIM = 768
+
+## Source for database timestamp: https://stackoverflow.com/questions/13370317/sqlalchemy-default-datetime
+class utcnow(expression.FunctionElement):
+    type = DateTime()
+    inherit_cache = True
+
+@compiles(utcnow, 'postgresql')
+def pg_utcnow(element, compiler, **kw):
+    return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
+
+class TextEmbedding(Base):
+    __tablename__ = 'text_embeddings'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    text = Column(String)
+    embedding = mapped_column(Vector(N_DIM))
+    created_at = Column(DateTime(timezone=True), server_default=utcnow())
+    updated_at = Column(DateTime(timezone=True), server_default=utcnow(), onupdate=utcnow())
+    
+    def __str__(self):
+        output = ''
+        for c in self.__table__.columns:
+            if c != "embedding":
+                output += '{}: {}\n'.format(c.name, getattr(self, c.name))
+        return output
+
+
+def insert_embeddings(embeddings, session):
+    for embedding in embeddings:
+        new_embedding = TextEmbedding(id = embedding["id"], text=embedding["text"], embedding=embedding["vector"])
+        session.add(new_embedding)
+    session.commit()
+
+def find_similar_embeddings(query_embedding, limit=5):
+    k = 5
+    similarity_threshold = 0.7
+    query = session.query(TextEmbedding, TextEmbedding.embedding.cosine_distance(query_embedding) \
+                    .label("distance")) \
+                    .filter(TextEmbedding.embedding.max_inner_product(query_embedding) < similarity_threshold) \
+                    .order_by("distance") \
+                    .limit(k) \
+                    .all()
+    return query
+
+def main():
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', filename='vector_database_seeding.log', level=logging.DEBUG)
+    logger.info('Started')
+
+    # Encode data into vector embedding
+    df = pd.read_json(os.path.join(DATAPATH, FILENAME), lines=True)
+
+    model = SentenceTransformer(MODEL)
+
+    # Encode the data
+    start = time.time()
+
+    # Timed code
+    encoded_data = model.encode(
+        df.statement.tolist(),
+    )
+    
+
+    end = time.time()
+    elapsed = end - start
+    logger.info(f"elapsed time to encode data: {elapsed}")
+    logger.info(f"shape of encoded_data: {encoded_data.shape}")
+    
+    docs = df.statement.values
+    data = [
+        {"id": i, "vector": encoded_data[i], "text": docs[i]}
+        for i in range(len(encoded_data))
+    ]
+    logger.info(f"Length of seeding data: {len(data)}")
+    
+    # Connect to PostgreSQL
+    engine = create_engine('postgresql://vector:password@db:5432/vectordb')
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.execute(text('CREATE EXTENSION IF NOT EXISTS vector'))
+    
+    # create table
+    Base.metadata.create_all(engine)
+
+    # Insert embeddings to database
+    start = time.time()
+    
+    insert_embeddings(data, session)
+    
+    end = time.time()
+    
+    logger.info(f"elapsed time to insert vector embedding data: {end - start}")
+
+    logger.info('Finished')
+
+
+if __name__ == "__main__":
+    main()
