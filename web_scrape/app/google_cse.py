0db75575801c682aa04d94c2a654b24ad8b0df91
SophieTruong <sophietruong1992@gmail.com>
2025-02-27 02:35:08 +0200

    Improve Google CSE search: remove code duplicates for CSE factchecking sites

---
+++ web_scrape/app/google_cse.py
@@ -0,0 +1,120 @@
+import os
+from dotenv import load_dotenv
+import json
+import requests
+
+from datetime import datetime
+
+from url_builder import URLBuilder
+from utils import get_timestamp, get_meta_value, is_article_after_timestamp, is_valid_datetime
+from factchecked_data import GoogleCustomSearchEngine, FactCheckOrg, FullFact, Snopes
+from typing import List, Optional
+
+load_dotenv()
+
+CSE_ID = {
+    "factcheckorg": os.getenv("CSE_ID_FCO"),
+    "fullfact": os.getenv("CSE_ID_FF"),
+    "snopes": os.getenv("CSE_ID_Snopes"),
+}
+
+def get_url(query:str, se_key=None, se_id=None):
+
+    builder = URLBuilder()
+
+    # .add_param("sort", "") = sort by Relevance
+    url = builder.set_scheme("https") \
+                .set_authority("www.googleapis.com/") \
+                .set_path("customsearch/v1") \
+                .add_param("key",se_key) \
+                .add_param("cx",se_id) \
+                .add_param("q",query) \
+                .add_param("sort", "") \
+                .build()
+    
+    return url
+
+def get_json_response(url, timestamp: datetime, source: str) -> List[GoogleCustomSearchEngine]:
+    
+    try:
+        response = requests.get(url)
+
+        # response_json = json.loads(response.text)
+        response_json = response.json()
+        
+        response_items = response_json.get("items", [])
+        
+        if not response_items:
+            return []  
+        
+        else:
+            return filter_google_cse_results(response_items, timestamp, source)
+            
+    except Exception as err:
+        print(f"Error when parsing response json: {err}")
+        return []
+
+def filter_google_cse_results(
+    response_items: List[dict], 
+    timestamp: datetime, 
+    source: str = "Google Custom Search Engine"
+    ) -> List[GoogleCustomSearchEngine]:
+    
+    # Process items
+    response_data = []
+    for item in response_items:
+        article_published_time = get_meta_value(item, "article:published_time")
+        article_modified_time = get_meta_value(item, "article:modified_time")
+        
+        # Skip if article is too new
+        if any(
+            is_article_after_timestamp(time, timestamp) 
+            for time in [article_published_time, article_modified_time]
+            if is_valid_datetime(time)
+        ):
+            continue
+        
+        cse_result = GoogleCustomSearchEngine(
+            title = item["title"],
+            source = source,
+            author = get_meta_value(item, "author"),
+            snippet = item["snippet"],
+            link = item["link"],
+            article_published_time = article_published_time,
+            article_modified_time = article_modified_time
+        )
+        response_data.append(cse_result)
+    
+    return response_data
+
+
+def get_cse_search_results(
+    query: str, 
+    source: str,
+    timestamp: Optional[str] = None
+    ) -> List[FactCheckOrg | FullFact | Snopes]:
+    try:
+        if source not in CSE_ID.keys():
+            raise ValueError(f"Invalid source: {source}, Source must be one of the following: {CSE_ID.keys()}")
+        
+        timestamp = get_timestamp(timestamp)
+        print(f"Getting {source} search results for {query} at {timestamp}")
+
+        cse_api_key = os.getenv("CSE_API_KEY")
+        
+        cse_id = CSE_ID[source]
+        
+        url = get_url(query, cse_api_key, cse_id)        
+        
+        response = get_json_response(url, timestamp, source)
+        
+        if source == "factcheckorg":
+            return [FactCheckOrg(**item) for item in response]
+        elif source == "fullfact":
+            return [FullFact(**item) for item in response]
+        else:
+            return [Snopes(**item) for item in response]
+            
+    except Exception as e:
+        print(f"Error getting {source} search results for {query}: {e}")
+        return []
--- web_scrape/app/main.py
+++ web_scrape/app/main.py
@@ -1,11 +1,11 @@
 import fastapi
 from model import SearchInput, SearchResponse, SingleSearchResult
-from fullfact import get_fullfact_search_results
 from politifact import get_politifact_search_results
-from faktabaari import get_faktabaari_search_results
-from factcheckorg import get_factcheckorg_search_results
+# from faktabaari import get_faktabaari_search_results
+from google_cse import get_cse_search_results
 from translator import translate_claim
 
+import time
 app = fastapi.FastAPI()
 
 @app.get("/")
@@ -14,14 +14,26 @@ def read_root():
 
 @app.post("/search", response_model=SearchResponse)
 def search(search_input: SearchInput):
+    
+    start_time = time.time()
+    
     responses = []
     for claim in search_input.claims:
+        print(f"Original Claim: {claim}")
+        
+        english_claim = translate_claim(claim.claim, "en")
+        print(f"Translated claim: {english_claim}")
+
         result_dict = {
-            # "faktabaari": get_faktabaari_search_results(translate_claim(claim, "fi")), # FaktaBaari: query in Finnish
-            "politifact": get_politifact_search_results(translate_claim(claim, "en")), # PoliticFactData: query in English
-            "factcheck.org": get_factcheckorg_search_results(translate_claim(claim, "en")), # FactCheckOrg: query in English
-            "fullfact": get_fullfact_search_results(translate_claim(claim, "en")) # FullFact
+            "politifact": get_politifact_search_results(english_claim, claim.timestamp),
+            "factcheck.org": get_cse_search_results(english_claim, "factcheckorg", claim.timestamp),
+            "fullfact": get_cse_search_results(english_claim, "fullfact", claim.timestamp),
+            "snopes": get_cse_search_results(english_claim, "snopes", claim.timestamp),
         }
         
-        responses.append(SingleSearchResult(claim=claim, response=result_dict))
+        responses.append(SingleSearchResult(claim=claim.claim, response=result_dict))
+    
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
+    
     return SearchResponse(results=responses)
