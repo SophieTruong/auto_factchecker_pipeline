558a6d664178f90f1bc7fe24bd74091c0178ed27
SophieTruong <sophietruong1992@gmail.com>
2025-02-27 02:35:51 +0200

    Add timestamp as search input

--- web_scrape/app/model.py
+++ web_scrape/app/model.py
@@ -1,15 +1,19 @@
 from typing import Dict, List, Union
 from pydantic import BaseModel
-from factchecked_data import PoliticFactData, FaktaBaari, FactCheckOrg, FullFact
+from factchecked_data import PoliticFactData, FaktaBaari, FactCheckOrg, FullFact, Snopes
 
 # Define a union type for all possible fact-check types
-FactCheckResult = Union[PoliticFactData, FaktaBaari, FactCheckOrg, FullFact]
+FactCheckResult = Union[PoliticFactData, FaktaBaari, FactCheckOrg, FullFact, Snopes]
 
 # Define the response type
 SourceSearchResults = Dict[str, List[FactCheckResult]]
 
+class Claim(BaseModel):
+    claim: str
+    timestamp: str
+
 class SearchInput(BaseModel):
-    claims: List[str]
+    claims: List[Claim]
 
 class SingleSearchResult(BaseModel):
     claim: str
