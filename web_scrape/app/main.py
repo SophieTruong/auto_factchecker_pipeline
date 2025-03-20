9f20a14d38050586dd292529160e33d072fa3ae2
SophieTruong <sophietruong1992@gmail.com>
2025-03-20 12:42:28 +0200

    Remove unnessary import

--- web_scrape/app/main.py
+++ web_scrape/app/main.py
@@ -2,22 +2,16 @@ import fastapi
 
 from model import Claim, WebScrapResult
 
-from politifact import get_politifact_search_results
-# from faktabaari import get_faktabaari_search_results
-from google_cse import get_cse_search_results
-from translator import translate_claim
-
 from concurrent_search import concurrent_search
 
 from utils import logger
-
-import time
 
 app = fastapi.FastAPI()
 
 @app.post("/search", response_model=WebScrapResult)
 async def search(search_input: Claim):
-        return WebScrapResult(
+    logger.info(f"Received search request for claim: {search_input.claim}")
+    return WebScrapResult(
         claim=search_input.claim, 
         response=await concurrent_search(search_input)
-        )
+    )
