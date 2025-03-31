9716980eeb070c104d49488069dfa97d85f16ec3
Thomas Pfau <github@thomaspfau.de>
2025-03-31 12:42:22 +0300

    Adding docker build script

---
+++ helm/buildContainers.sh
@@ -0,0 +1,16 @@
+cd ../api_service
+docker build -t harbor.cs.aalto.fi/aaltorse-public/api_service:latest .
+cd ../data_retrieval 
+docker build -t harbor.cs.aalto.fi/aaltorse-public/data_retrieval:latest .
+cd ../evidence_retrieval
+docker build -t harbor.cs.aalto.fi/aaltorse-public/evidence_retrieval:latest .
+cd ../milvus_standalone
+docker build -t harbor.cs.aalto.fi/aaltorse-public/db_seed:latest .
+cd ../model_inference_server
+docker build -t harbor.cs.aalto.fi/aaltorse-public/model_inference:latest .
+cd ../model_monitoring_service
+docker build -t harbor.cs.aalto.fi/aaltorse-public/model_monitoring:latest .
+docker build -f app/rabbitmq_consumer/Dockerfile -t harbor.cs.aalto.fi/aaltorse-public/rabbitmq_consumer:latest .
+cd ../../web_scrape
+docker build -t harbor.cs.aalto.fi/aaltorse-public/web_scrape:latest .
+cd ../helm
