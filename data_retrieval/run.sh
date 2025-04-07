d3fe2ec779c8d2375fa7f6fb533bd69a8914b5b5
Thomas Pfau <github@thomaspfau.de>
2025-04-07 09:17:43 +0300

    Update HF data retrieval

--- data_retrieval/run.sh
+++ data_retrieval/run.sh
@@ -2,5 +2,5 @@
 
 downloadDataFolder $MODEL_DIR $MODEL_URL
 downloadDataFolder $NEWS_DATA_DIR $NEWS_DATA_URL
-downloadDataFolder $HF_FOLDER $HF_URL
 downloadDataFolder $FACEPAGER_DIR $FACEPAGER_URL
+python retrieveModel.py
